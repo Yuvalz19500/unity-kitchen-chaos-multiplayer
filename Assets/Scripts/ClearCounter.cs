ca36a4a9bf29eb2264034e53513ccd870b370863
Yuvalz19500 <yuvalz19500@gmail.com>
2024-11-28 18:18:10 +0200

    Add KitchenObject and ClearCounter scripts, implement interaction logic, and create associated prefab and scriptable object assets

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,7 +2,44 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private GameObject counterTopPoint;
+
+    private KitchenObject _kitchenObject;
+
     public void Interact()
     {
+        if (_kitchenObject == null)
+        {
+            GameObject kitchenObject = Instantiate(kitchenObjectSO.prefab, counterTopPoint.transform.position,
+                Quaternion.identity);
+
+            kitchenObject.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint.transform;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        _kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return _kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        _kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return _kitchenObject != null;
     }
 }
