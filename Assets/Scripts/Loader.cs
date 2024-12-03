455a1e4db9185bcfd56dc42b296564770e8f5e49
Yuvalz19500 <yuvalz19500@gmail.com>
2024-12-03 14:14:15 +0200

    Add Loader and MainMenuUI scripts; implement scene loading functionality and main menu interactions

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scenes
+    {
+        MainMenuScene,
+        GameScene,
+        LoadingScene
+    }
+
+    private static Scenes _targetScene;
+
+    public static void Load(Scenes targetScene)
+    {
+        _targetScene = targetScene;
+
+        SceneManager.LoadScene(Scenes.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(_targetScene.ToString());
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button quitButton;
+
+        private void Awake()
+        {
+            playButton.onClick.AddListener(() => { Loader.Load(Loader.Scenes.GameScene); });
+            quitButton.onClick.AddListener(Application.Quit);
+        }
+    }
+}
