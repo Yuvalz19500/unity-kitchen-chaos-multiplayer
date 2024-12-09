ff89a112bf7e897887e57a499b868ceb3e9734f0
Yuvalz19500 <yuvalz19500@gmail.com>
2024-12-09 14:24:44 +0200

    Add server-client synchronization for ingredient addition in PlateKitchenObject; implement ServerRpc and ClientRpc methods to ensure network consistency for kitchen object interactions

--- Assets/Scripts/KitchenObject/PlateKitchenObject.cs
+++ Assets/Scripts/KitchenObject/PlateKitchenObject.cs
@@ -1,31 +1,47 @@
 using System;
 using System.Collections.Generic;
 using ScriptableObjects;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
-    [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList; 
-    
+    [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+
     private readonly List<KitchenObjectSO> _kitchenObjectsSOList = new();
-    
+
     public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+
     public class OnIngredientAddedEventArgs : EventArgs
     {
         public KitchenObjectSO KitchenObjectSO;
     }
-    
+
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
-        if(!validKitchenObjectSOList.Contains(kitchenObjectSO)) return false;
-        if(_kitchenObjectsSOList.Contains(kitchenObjectSO)) return false;
-        
-        _kitchenObjectsSOList.Add(kitchenObjectSO);
-        
-        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { KitchenObjectSO = kitchenObjectSO });
+        if (!validKitchenObjectSOList.Contains(kitchenObjectSO)) return false;
+        if (_kitchenObjectsSOList.Contains(kitchenObjectSO)) return false;
+
+        AddIngredientServerRpc(KitchenGameMultiplayer.Instance.GetKitchenObjectSOIndex(kitchenObjectSO));
         return true;
     }
-    
+
+    [ServerRpc(RequireOwnership = false)]
+    private void AddIngredientServerRpc(int kitchenObjectSOIndex)
+    {
+        AddIngredientClientRpc(kitchenObjectSOIndex);
+    }
+
+    [ClientRpc]
+    private void AddIngredientClientRpc(int kitchenObjectSOIndex)
+    {
+        KitchenObjectSO kitchenObjectSO =
+            KitchenGameMultiplayer.Instance.GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
+        _kitchenObjectsSOList.Add(kitchenObjectSO);
+
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { KitchenObjectSO = kitchenObjectSO });
+    }
+
     public List<KitchenObjectSO> GetKitchenObjectsSOList()
     {
         return _kitchenObjectsSOList;
